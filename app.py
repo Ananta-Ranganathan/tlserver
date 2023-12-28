34f83ff00768697ac90b255162b8f12aabc7bd1f
Ananta Ranganathan <anantadr@gmail.com>
2023-12-28 13:48:12 -0800

    asdf

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from translate import translate
+import time
+import asyncio
+import os
+import subprocess
+
+
+app = Flask(__name__)
+CORS(app)
+i = 0
+
+@app.route('/translate', methods=['POST'])
+def translate_file():
+    global i
+    start = time.time()
+    print("Request received")
+    file = request.files.get('file')
+    if not file:
+        print("No file found in the request")
+        return jsonify({"error": "No file found"}), 400
+
+    original_file_path = f"original{i}.webm"
+    converted_file_path = f"converted{i}.wav"
+    i += 1
+    try:
+        file.save(original_file_path)
+        print(f"Original file saved at {original_file_path} with size {os.stat(original_file_path).st_size}")
+    except Exception as e:
+        print(f"Error saving file: {e}")
+        return jsonify({"error": "Error saving file"}), 500
+
+    # Convert the file using FFmpeg
+    try:
+        subprocess.run(["ffmpeg", "-y", "-f", "webm", "-i", original_file_path, converted_file_path], check=True)
+        print(f"File converted to {converted_file_path}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error during file conversion: {e}")
+        return jsonify({"error": "Error during file conversion"}), 500
+
+    # Ensure the converted file exists
+    if not os.path.exists(converted_file_path):
+        print(f"Converted file {converted_file_path} does not exist")
+        return jsonify({"error": "File not found after conversion"}), 500
+
+    print(f"Converted file size: {os.path.getsize(converted_file_path)} bytes")
+
+    # Process the converted file for translation
+    try:
+        translated_text = asyncio.run(translate(converted_file_path))
+        print(translated_text)
+    except Exception as e:
+        print(f"Error during translation: {e}")
+        return jsonify({"error": "Error during translation"}), 500
+
+    print("Time taken for translation: ", time.time() - start)
+    return jsonify({"translatedText": translated_text})
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True, threaded=True)
---
+++ translate.py
@@ -0,0 +1,27 @@
+from transformers import SeamlessM4Tv2Model, AutoProcessor
+import torchaudio
+import asyncio
+
+processor = AutoProcessor.from_pretrained("facebook/seamless-m4t-v2-large")
+model = SeamlessM4Tv2Model.from_pretrained("facebook/seamless-m4t-v2-large")
+
+
+async def translate(file):
+    print("Translating")
+    audio, orig_freq = torchaudio.load(file)
+    print("Audio loaded")
+    audio =  torchaudio.functional.resample(audio, orig_freq=orig_freq, new_freq=16_000)
+    print("Audio resampled")
+    audio_inputs = processor(audios=audio, return_tensors="pt", sampling_rate=16_000)
+    print("Audio processed")
+    output_tokens = model.generate(**audio_inputs, tgt_lang="eng", generate_speech=False)
+    print("Translation generated")
+    transcription = processor.decode(output_tokens[0].tolist()[0], skip_special_tokens=True)
+    print("Transcription decoded")
+    return transcription
+
+async def main():
+    print(await translate("test.wav"))
+
+if __name__ == "__main__":
+    asyncio.run(main())
